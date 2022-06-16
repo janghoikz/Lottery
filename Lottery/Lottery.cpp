abdfebc595787d14bf11ff3eecc59b60317aa938
ohne <ghdlr785@gmail.com>
2022-06-16 14:10:00 +0900

    초기 커밋.

---
+++ Lottery/Lottery.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void Swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void Sort(int numbers[], int count)
+{
+    int temp;
+    for (int i = 0; i < count; i++)
+    {
+        int best = i;
+        for (int j = i + 1; j < count; j++)
+        {
+            if (numbers[j] < numbers[best])
+            {
+                best = j;
+            }
+        }
+        if (i != best)
+        {
+            Swap(numbers[i], numbers[best]);
+        }
+    }
+}
+
+void ChooseLotto(int numbers[])
+{
+    srand((unsigned)time(0));
+    
+    int count = 0;
+    while (count != 6)
+    {
+        int randValue = 1 + (rand() % 45);
+
+
+        bool found = false;
+        for (int i = 0; i < count; i++)
+        {
+            if (numbers[i] == randValue)
+            {
+                found = true;
+                break;
+            }
+        }
+
+        if (found == false)
+        {
+            numbers[count] = randValue;
+            count++;
+        }
+    }
+
+    Sort(numbers, 6);
+}
+
+int main()
+{
+    int a = 1;
+    int b = 2;
+    Swap(a, b);
+    cout << a << b << endl;
+
+    int numbers[6] = { 1, 42, 3, 15, 5, 6 };
+    Sort(numbers, 6);
+
+    ChooseLotto(numbers);
+
+    for (int i = 0; i < 6; i++)
+    {
+        cout << numbers[i] << endl;
+    }
+}
+
